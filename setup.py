2ed325ee545fa0c62c568e2cbf9246fa4b5db360
virbickt <63246400+virbickt@users.noreply.github.com>
2021-02-26 13:27:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
+from setuptools import setup
 
+setup(
+  name="tesla-factory",
+  version="0.0.1",
+  description="Make your own Tesla",
+  py_modules=["factory.py"],
+  package_dir={"": "src"},
+)
